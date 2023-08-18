5c1ba68125cb1f6220d28848a0b2c60fedbc3ed5
AndressaSRodrigues <srandressa2@gmail.com>
2023-08-18 15:26:20 -0400

    actualize ./src

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ command }) => {
   if (command === 'build') {
     return {
-      root: 'src',
+      root: './src',
       // para hacer un deploy en github pages, configura propiedad base con el
       // nombre/url de tu repo
       // para fazer uma implantação github pages, defina a propriedade base
